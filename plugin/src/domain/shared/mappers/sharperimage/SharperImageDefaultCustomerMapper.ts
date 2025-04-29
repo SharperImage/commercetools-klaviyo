988ff9e2860683db989685d26d55bf2c932b64b5
KthProg <kyle.thomas.hooks@gmail.com>
2025-04-29 09:44:20 -0400

    handle undefined phone number

--- plugin/src/domain/shared/mappers/sharperimage/SharperImageDefaultCustomerMapper.ts
+++ plugin/src/domain/shared/mappers/sharperimage/SharperImageDefaultCustomerMapper.ts
@@ -10,12 +10,19 @@ export class SharperImageDefaultCustomerMapper extends DefaultCustomerMapper imp
     }
 
     // phone format: +12345678901
-    const phone10Digit = (address.phone ?? '').replace(/[^0-9]+/gi, '').slice(-10).padStart(10, '0');
+    const phone10Digit = address.phone ? address.phone.replace(/[^0-9]+/gi, '').slice(-10).padStart(10, '0') : null;
 
+    if(phone10Digit){
+      return {
+        firstName: address.firstName,
+        lastName: address.lastName,
+        phoneNumber: `+1${phone10Digit}`,
+      };
+    }
+    
     return {
       firstName: address.firstName,
       lastName: address.lastName,
-      phoneNumber: `+1${phone10Digit}`,
     };
   }
 }
